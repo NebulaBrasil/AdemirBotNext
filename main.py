c7d371b57c2beebe9119c53b74388ccde30b223e
Emmanuel Pereira <emmanuel@timais.com>
2023-07-13 19:36:36 -0300

    fix: Versão do discord.py do github (assincrona)

--- main.py
+++ main.py
@@ -9,21 +9,15 @@ intents.members = True
 
 bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
 
+async def load_cogs():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
 @bot.event
 async def on_ready():
     print(f'Bot conectado como {bot.user.name}')
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-# Carregar os cogs
-initial_extensions = [
-    'cogs.gpt_assistant_cog'
-]
+    await load_cogs()
 
 if __name__ == '__main__':
-    for extension in initial_extensions:
-        bot.load_extension(extension)
-
     bot.run(config.TOKEN)
