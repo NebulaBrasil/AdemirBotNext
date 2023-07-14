74b81822c5a9b912e0bcf0b843ac060e417f97c2
Emmanuel Pereira <emmanuel@timais.com>
2023-07-13 21:03:10 -0300

    improve: logging

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-# main.py
+import logging
 import asyncio
 import os
-import interactions
 import config
+from interactions import Client, Intents, listen
 
-client = interactions.Client()
+logging.basicConfig()
+cls_log = logging.getLogger("Ademir")
+cls_log.setLevel(logging.DEBUG)
 
-@interactions.listen()
+client = Client(intents=Intents.ALL, sync_interactions=True, asyncio_debug=True, logger=cls_log)
+
+@listen()
 async def on_startup():
     print("Bot ready")
     
