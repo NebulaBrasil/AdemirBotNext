eedd82a4092cf155d24166b7eb84cb16b570defc
Emmanuel Pereira <emmanuel@timais.com>
2023-07-13 22:35:32 -0300

    improve: exemplo de uso com eventos de mensagens

--- extensions/gpt_assistant.py
+++ extensions/gpt_assistant.py
@@ -8,6 +8,16 @@ class GptAssistant(interactions.Extension):
     def __init__(self, client: interactions.Client) -> None:
         self.client: interactions.Client = client
     
+    @interactions.listen()
+    async def on_message_create(self, message_create: interactions.events.MessageCreate):
+        message: interactions.Message = message_create.message        
+        if message.client.user.mention in message.content:                
+            response = openai.ChatCompletion.create(
+                model=config.CHAT_GPT_MODEL,
+                messages=[{ "role": "user", "content": str.replace(message.content, message.client.user.mention, config.NOME_BOT) }]
+                )
+            await message.channel.send(response["choices"][0]["message"]["content"])
+
     @interactions.slash_command("hello", description="Say hello!")
     async def hello(self, ctx: interactions.SlashContext):
         await ctx.send("This command is ran inside an Extension")
--- main.py
+++ main.py
@@ -8,18 +8,17 @@ logging.basicConfig()
 cls_log = logging.getLogger("Ademir")
 cls_log.setLevel(logging.DEBUG)
 
-client = Client(intents=Intents.ALL, sync_interactions=True, asyncio_debug=True, logger=cls_log)
+client = Client(intents=Intents.ALL, sync_interactions=True, logger=cls_log)
 
 @listen()
 async def on_startup():
     print("Bot ready")
     
-async def load_cogs():
+def load_extensions():
     for filename in os.listdir('./extensions'):
         if filename.endswith('.py'):
             client.load_extension(f'extensions.{filename[:-3]}')
   
 if __name__ == '__main__':
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(load_cogs())   
+    load_extensions() 
     client.start(config.TOKEN)
