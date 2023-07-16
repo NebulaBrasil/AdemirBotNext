46076ea1bfcbd31fbb55f63d596e7b402231a9e0
Emmanuel Pereira <emmanuel@timais.com>
2023-07-16 19:50:37 -0300

    tests: config-cargo-ademir

--- extensions/config_manage.py
+++ extensions/config_manage.py
@@ -1,12 +1,11 @@
 import interactions
-from interactions import Extension, OptionType, Role, slash_option
+from interactions import Client, Extension, OptionType, slash_option
 from repository.ademir_cfg_repository import AdemirCfgRepository
 
 class ConfigManage(Extension):
-    def __init__(self, client: interactions.Client) -> None:
-        self.client: interactions.Client = client        
-        self.repo = AdemirCfgRepository()
-
+    def __init__(self, client: Client) -> None:
+        self.client: Client = client
+        
     @interactions.slash_command(
         name="config-cargo-ademir", 
         description="Configurar cargo que pode falar com o Ademir.",
@@ -18,8 +17,9 @@ class ConfigManage(Extension):
         required=True,
         opt_type=OptionType.ROLE
     )
-    async def configure_conversation_role(self, ctx: interactions.SlashContext, cargo: Role):
-        await ctx.defer()
+    async def configure_conversation_role(self, ctx: interactions.SlashContext, cargo):
+        await ctx.defer()   
+        self.repo = AdemirCfgRepository()
         self.repo.set_guild_conversation_role(ctx.guild_id, cargo.id)
         await ctx.send(f"Cargo {cargo.mention} permitido para o Ademir configurado.", ephemeral=True)
 
